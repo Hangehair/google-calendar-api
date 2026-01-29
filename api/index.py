3a926dbc02e5ed2dda9df6938406c5246cbbb580
Hangehair <151835234+Hangehair@users.noreply.github.com>
2026-01-30 06:22:32 +0800

    更新 index.py

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
 SCOPES = ['https://www.googleapis.com/auth/calendar']
-CALENDAR_ID = os.getenv('CALENDAR_ID')
+CALENDAR_ID = os.getenv('winter81943@yahoo.com.tw')
 
 class handler(BaseHTTPRequestHandler):
     def _set_headers(self, status=200):
